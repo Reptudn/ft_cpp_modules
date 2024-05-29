fa2bf72cdf8001f56381b5f9e592102710e041a1
jkauker <jkauker@student.42heilbronn.de>
2024-05-29 10:37:16 +0200

    fixed makefiles to not relinl

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -3,34 +3,6 @@
 #include <ostream>
 #include <string>
 
-std::string strtrim(char *str, char c)
-{
-	unsigned long start_point = 0;
-
-	char	*start	= str;
-	while (std::isspace(*start))
-	{
-		start_point++;
-		start++;
-	}
-
-	unsigned long end = 0;
-	for (int i = 0; str[i]; i++)
-	{
-		end++;
-		str++;
-	}
-
-	while (std::isspace(*str))
-	{
-		str--;
-		end--;
-	}
-
-	std::string new_str = std::to_string(str);
-	return (new_str.substr(start_point, end))
-}
-
 int main(int argc, char **argv)
 {
 	if (argc == 1)
