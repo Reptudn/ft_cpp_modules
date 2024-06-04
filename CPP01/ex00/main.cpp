d76b93481bfbc0effc81aba750cf7668f7dd040a
Jonas <jonas.kauker2003@gmail.com>
2024-06-04 09:58:05 +0200

    CPP01 ex01
    - added newZombie and randomChump Methods

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "Zombie.hpp"
 
+void	randomChump(std::string name)
+{
+	Zombie newZombie(name);
+	newZombie.announce();
+}
+
+Zombie* newZombie(std::string name)
+{
+	return new Zombie(name);
+}
+
 int main ()
 {
+	Zombie* zombie = newZombie("Zombie");
+	zombie->announce();
+
+	randomChump("Chump");
 	return 0;
 }
