2461e5b955616c2f311bb93b1f631763a4c9761b
jkauker <jkauker@student.42heilbronn.de>
2024-08-07 11:26:58 +0200

    added get and set Raw bit function loggind + added copy constructor in cpp02 ex00

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkauker <jkauker@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 19:04:17 by jkauker           #+#    #+#             */
-/*   Updated: 2024/07/18 21:07:06 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/08/07 11:25:00 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,14 @@
 
 Fixed::Fixed()
 {
+	this->_value = 0;
 	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &old)
+{
+	*this = old;
+	std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
@@ -25,17 +32,18 @@ Fixed::~Fixed()
 
 int Fixed::getRawBits(void) const
 {
+	std::cout << "getRawBits member function called" << std::endl;
 	return this->_value;
 }
 
 void Fixed::setRawBits(int const raw)
 {
+	std::cout << "setRawBits member function called" << std::endl;
     this->_value = raw;
 }
 
 Fixed& Fixed::operator=(Fixed const &fixed)
 {
 	this->_value = fixed._value;
-	this->_fractionalBits = fixed._fractionalBits;
 	return *this;
 }
