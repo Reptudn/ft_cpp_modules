12e05b6acc031c62d02c2b8919adf87160f02449
Jonas <87537789+Reptudn@users.noreply.github.com>
2024-09-12 16:19:47 +0200

    Update MutantStack.hpp

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include <stack>
 
@@ -7,17 +6,17 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack() : std::stack<T>() {};
+		MutantStack() : std::stack<T>() { };
 		~MutantStack() = default;
-		MutantStack(const MutantStack &other) : std::stack<T>(other)) {};
-		MutantStack &operator=(const MutantStack &other)
+		MutantStack(const MutantStack<T> &other) : std::stack<T>(other) { };
+		MutantStack &operator=(const MutantStack<T> &other)
 		{
-			if (this == &other) return *this;
+			if (this == *other) return *this;
 			this->c = other.c;
 			return *this;
 		};
 
-		typedef std::stack<T>::container_type::iterator iterator;
-		iterator *start() { return this->c.begin(); };
-		iterator *end() { return this->c.end(); };
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() { return this->c.begin(); };
+		iterator end() { return this->c.end(); };
 };
