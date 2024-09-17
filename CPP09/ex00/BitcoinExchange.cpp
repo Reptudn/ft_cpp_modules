1f8d52dc5790dce6ac9e68e33e3e6c8ca15a33b3
jkauker <jkauker@student.42heilbronn.de>
2024-09-17 13:16:10 +0200

    fixed datacsv having \r caracters in the file

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -7,11 +7,16 @@
 BitcoinExchange::BitcoinExchange(std::string filename)
 {
 	_filename = filename;
-	if (!isFileValid())
-		throw std::runtime_error("There is an error with the file.");
+	if (!isFileValid("data.csv", std::regex(R"(\d{4}-\d{2}-\d{2},\d+(\.\d*)?(\r?\n)?$)")))
+		throw std::runtime_error("There is an error with the db file.");
 	else
-	 	std::cout << "File: OK!" << std::endl;
+	 	std::cout << "File db: OK!" << std::endl;
 	
+	if (!isFileValid(filename, std::regex(R"(\d{4}-\d{2}-\d{2} \| \d+(\.\d+)?$)")))
+		throw std::runtime_error("There is an error with the input file.");
+	else
+	 	std::cout << "File input: OK!" << std::endl;
+
 	int i = 0;
 	std::string line;
 	std::fstream file(_filename);
@@ -38,37 +43,29 @@ BitcoinExchange::~BitcoinExchange()
 {
 }
 
-bool BitcoinExchange::isFileValid() const
+bool BitcoinExchange::isFileValid(std::string filename, std::regex pattern) const
 {
-	std::fstream file;
-	file.open(_filename, std::ios::in);
+	std::ifstream file(filename);
 	if (!file.is_open() || !file.good())
 	{
-		std::cerr << "Error: File " << _filename << " does not exist." << std::endl;
+		std::cerr << "Error: File " << filename << " does not exist." << std::endl;
 		return false;
 	}
-
-	std::regex pattern(R"(\d{4}-\d{2}-\d{2},\d+(\.\d*)?(\r?\n)?$)");
-
-	// why does this work here but not down in the loop?
-	std::string lline = "2009-01-02,0\n";
-	if (!std::regex_match(lline, pattern))
-	{
-		std::cerr << "Error: Line '" + lline + "' doesn't match the right format" << std::endl;
-	} else std::cout << "matches the pattern" << std::endl;
 
 	int i = 0;
 	std::string line;
 	while (std::getline(file, line))
 	{
-		std::cout << ">" << line << "<";
-		if (i != 0 && !line.empty() && !std::regex_match(line, pattern))
+		// std::cout << "Line " << i << ": " << line << std::endl;
+		if (i != 0 && !std::regex_match(line, pattern))
 		{
-			std::cerr << " doesn't match the right format" << std::endl;s
+			std::cerr << " doesn't match the right format" << std::endl;
+			file.close();
 			return false;
-		} else std::cerr << " matches the pattern" << std::endl;
+		}
 		i++;
 	}
+	file.close();
 	return true;
 }
 
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -5,14 +5,16 @@
 #include <vector>
 #include <ctime>
 #include <utility>
+#include <regex>
 
 class BitcoinExchange
 {
 	private:
 		std::string _filename;
+		std::string _db;
 		std::vector<std::pair<std::tm, float>> _data;
 
-		bool isFileValid() const;
+		bool isFileValid(std::string filename, std::regex pattern) const;
 	public:
 		BitcoinExchange(std::string filepath);
 		~BitcoinExchange();
