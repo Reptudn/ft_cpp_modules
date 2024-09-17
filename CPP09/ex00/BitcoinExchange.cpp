e6a0c396533b37565c4b748dd7ead9b06029a0e6
jkauker <jkauker@student.42heilbronn.de>
2024-09-17 16:13:30 +0200

    cpp09 ex00 done

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -7,6 +7,7 @@
 BitcoinExchange::BitcoinExchange(std::string filename)
 {
 	_filename = filename;
+	_db = "data.csv";
 	if (!isFileValid("data.csv", std::regex(R"(\d{4}-\d{2}-\d{2},\d+(\.\d*)?(\r?\n)?$)")))
 		throw std::runtime_error("There is an error with the db file.");
 	else
@@ -17,30 +18,112 @@ BitcoinExchange::BitcoinExchange(std::string filename)
 	else
 	 	std::cout << "File input: OK!" << std::endl;
 
+	try
+	{
+		loadDbFile();
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		throw std::runtime_error("There is an error with the db file.");
+	}
+
+	try
+	{
+		loadInputFile();
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		throw std::runtime_error("There is an error with the input file.");
+	}
+
+}
+
+void BitcoinExchange::loadInputFile() // YYYY-MM-DD | VALUE
+{
 	int i = 0;
 	std::string line;
 	std::fstream file(_filename);
+	file.seekg(0, std::ios::beg);
 	while (std::getline(file, line))
 	{
-		if (i == 0) continue;
+		if (i++ == 0) continue;
+
+		std::string date = line.substr(0, 10);
+		std::string value = line.substr(13, line.size());
+		std::tm tm = {};
+		strptime(date.c_str(), "%Y-%m-%d", &tm);
+
+		float val = std::stof(value);
+		if (val < 0.0 || val > 1000.0) 
+		{
+			file.close();
+			throw std::runtime_error("Value out of range");
+		}
+		_data.emplace(tm, val);
+	}
+	file.close();
+}
+
+void BitcoinExchange::loadDbFile() // YYYY-MM-DD,VALUE
+{
+	int i = 0;
+	std::string line;
+	std::fstream file(_db);
+	file.seekg(0, std::ios::beg);
+	while (std::getline(file, line))
+	{
+		if (i++ == 0) continue;
 
 		std::string date = line.substr(0, 10);
 		std::string value = line.substr(11, line.size());
 		std::tm tm = {};
 		strptime(date.c_str(), "%Y-%m-%d", &tm);
-		_data.push_back(std::make_pair(tm, std::stof(value)));
 
-		i++;
+		float val = std::stof(value);
+		// TODO: is the check for 0.0 and 1000.0 necessary?
+		// std::cout << "val is: " << std::showpoint << val << std::endl;
+		// if (val < 0.0 || val > 1000.0) 
+		// {
+		// 	file.close();
+		// 	throw std::runtime_error("Value out of range");
+		// }
+		_d.emplace((BitcoinData){tm, val});
 	}
+	file.close();
+}
+
+bool BitcoinExchange::isSameDate(std::tm date1, std::tm date2) const
+{
+	return date1.tm_year == date2.tm_year && date1.tm_mon == date2.tm_mon && date1.tm_mday == date2.tm_mday;
+}
+
+BitcoinCourse BitcoinExchange::getCourse(std::tm date) const
+{
+	BitcoinCourse course(date, 0);
+	auto it = _d.find(course);
+	if (it != _d.end())
+		return *it;
+	
+	auto it2 = _d.lower_bound(course);
+	if (it2 == _d.begin() || isSameDate(it2->date, date))
+		return *it2;
+	
+	return *(--it2);
 }
 
 void BitcoinExchange::printExchange() const
 {
-	
-}
+	auto it = _data.begin();
+	while (it != _data.end())
+	{
+		BitcoinCourse course = getCourse(it->date);
 
-BitcoinExchange::~BitcoinExchange()
-{
+		std::cout << std::put_time(&it->date, "%Y-%m-%d") << " => " << it->value << " = " << it->value * course.value << std::endl;
+
+		it++;
+	}
 }
 
 bool BitcoinExchange::isFileValid(std::string filename, std::regex pattern) const
@@ -59,7 +142,7 @@ bool BitcoinExchange::isFileValid(std::string filename, std::regex pattern) cons
 		// std::cout << "Line " << i << ": " << line << std::endl;
 		if (i != 0 && !std::regex_match(line, pattern))
 		{
-			std::cerr << " doesn't match the right format" << std::endl;
+			std::cerr << line << " doesn't match the right format" << std::endl;
 			file.close();
 			return false;
 		}
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -2,21 +2,53 @@
 
 #include <filesystem>
 #include <iostream>
-#include <vector>
+#include <set>
 #include <ctime>
 #include <utility>
 #include <regex>
 
-class BitcoinExchange
+typedef struct BitcoinData
+{
+	std::tm date;
+	float value;
+	bool operator<(const BitcoinData &data) const
+	{
+		time_t t1 = mktime(const_cast<std::tm*>(&date));
+		time_t t2 = mktime(const_cast<std::tm*>(&data.date));
+		return t1 < t2;
+	}
+	BitcoinData(std::tm date, float value) : date(date), value(value) {}
+} BitcoinData;
+
+typedef struct BitcoinCourse
+{
+	std::tm date;
+	float value;
+	bool operator<(const BitcoinCourse &course) const
+	{
+		time_t t1 = mktime(const_cast<std::tm*>(&date));
+		time_t t2 = mktime(const_cast<std::tm*>(&course.date));
+		return t1 < t2;
+	}
+	BitcoinCourse(std::tm date, float value) : date(date), value(value) {}
+	BitcoinCourse(const BitcoinData& data) : date(data.date), value(data.value) {}
+} BitcoinCourse;
+
+class BitcoinExchange	
 {
 	private:
 		std::string _filename;
 		std::string _db;
-		std::vector<std::pair<std::tm, float>> _data;
+		std::multiset<BitcoinData> _data;
+		std::multiset<BitcoinCourse> _d;
 
+		void loadInputFile();
+		void loadDbFile();
 		bool isFileValid(std::string filename, std::regex pattern) const;
+		bool isSameDate(std::tm date1, std::tm date2) const;
+		BitcoinCourse getCourse(std::tm date) const;
 	public:
 		BitcoinExchange(std::string filepath);
-		~BitcoinExchange();
+		~BitcoinExchange() = default;
 		void printExchange() const;
 };
